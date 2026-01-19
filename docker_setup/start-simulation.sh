4cd6187ef43f3d947c3362e02d2cfa07730da107
Sina Steinmüller <sina_steinmller@gmx.de>
2026-01-19 18:41:15 +0100

    changed color to green

--- docker_setup/start-simulation.sh
+++ docker_setup/start-simulation.sh
@@ -13,22 +13,22 @@ if [ -z "$TMUX" ]; then
   # Pane 1 (bottom left)
   tmux select-pane -t 1
   tmux select-pane -T "2. Start Virtual Joy"
-  tmux send-keys "PYTHONPATH=/home/user/python_env/.flet/lib/python3.12/site-packages:$PYTHONPATH ros2 run edu_virtual_joy virtual_joy --ros-args -r __ns:=/eduard/blue" C-m
+  tmux send-keys "PYTHONPATH=/home/user/python_env/.flet/lib/python3.12/site-packages:$PYTHONPATH ros2 run edu_virtual_joy virtual_joy --ros-args -r __ns:=/eduard/green" C-m
 
   # Pane 2 (top right)
   tmux select-pane -t 2
   tmux select-pane -T "3. Add Eduard"
   tmux send-keys "sleep 5 && \
   ros2 launch edu_simulation eduard.launch.py \
-  edu_robot_namespace:=eduard/blue \
+  edu_robot_namespace:=eduard/green \
   wheel_type:=mecanum \
-  eduard_color:=blue \
+  eduard_color:=green \
   pos_x:=0.0 pos_y:=0.0 pos_z:=0.04 yaw:=0.0" C-m
 
   # Pane 3 (bottom right)
   tmux select-pane -t 3
   tmux select-pane -T "4. Start RViz"
-  tmux send-keys "ros2 launch /home/user/workspace/ros2_ws/src/simulation/launch/robot_monitor.launch.py edu_robot_namespace:=eduard/blue" C-m
+  tmux send-keys "ros2 launch /home/user/workspace/ros2_ws/src/simulation/launch/robot_monitor.launch.py edu_robot_namespace:=eduard/green" C-m
 
   tmux select-pane -t 0
   tmux attach-session -t simulation
